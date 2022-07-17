438938a02e800211db12fddac2524d48308651e4
Din <dsianlly718@gmail.com>
2022-07-18 06:17:25 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui
+pyautogui.PAUSE = 1
+# pyautogui.displayMousePosition()
+# print(pyautogui.KEYBOARD_KEYS)
+
+
+# pyautogui.press("winleft")
+# pyautogui.typewrite("discord")
+# pyautogui.press("enter")
+
+pyautogui.press("winleft")
+pyautogui.typewrite("google chrome")
+pyautogui.press("enter")
+
+pyautogui.typewrite("youtube")
+pyautogui.press("enter")
