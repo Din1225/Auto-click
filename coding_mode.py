d9b2aab04f4441c9052ee19dcef5741592c7e448
Din <dsianlly718@gmail.com>
2022-07-18 19:37:18 +0800

    Update coding_mode.py

--- coding_mode.py
+++ coding_mode.py
@@ -1,6 +1,6 @@
 import pyautogui
 pyautogui.PAUSE = 0.8                 
-#寫code的程式
+# 開寫code的程式
 pyautogui.press("winleft")
 pyautogui.typewrite("visual studio code")
 pyautogui.press("enter")
@@ -10,17 +10,25 @@ pyautogui.typewrite("github desktop")
 pyautogui.press("enter")
 
 pyautogui.press("winleft")
+pyautogui.typewrite("cmd")
+pyautogui.press("enter")
+
+pyautogui.typewrite("jupyter notebook")
+
+# 開github
+pyautogui.press("winleft")
 pyautogui.typewrite("google chrome")
 pyautogui.press("enter")
 
 pyautogui.PAUSE = 1.5
-pyautogui.moveTo(561,6)
-pyautogui.click(561,6)
+pyautogui.moveTo(201,52)
+pyautogui.click(201,52)
 pyautogui.PAUSE = 0.8
 pyautogui.typewrite("https://github.com/Din1225?tab=repositories")
 pyautogui.press("enter")
 
-# pyautogui.press("winleft")
-# pyautogui.typewrite("discord")
-# pyautogui.press("enter")
+# discord
+pyautogui.press("winleft")
+pyautogui.typewrite("discord")
+pyautogui.press("enter")
 
