80d5dcca7a4ff212d939408a5bead8d048fba2d7
Din <dsianlly718@gmail.com>
2022-07-21 19:25:41 +0800

    Update coding_mode.py

--- coding_mode.py
+++ coding_mode.py
@@ -6,29 +6,32 @@ pyautogui.press("winleft")
 pyautogui.typewrite("visual studio code")
 pyautogui.press("enter")
 
+pyautogui.PAUSE = 20 
 pyautogui.press("winleft")
+pyautogui.PAUSE = 0.8  
 pyautogui.typewrite("github desktop")
 pyautogui.press("enter")
 
-pyautogui.PAUSE = 5  
+pyautogui.PAUSE = 20  
 pyautogui.press("winleft")
+pyautogui.PAUSE = 0.8  
 pyautogui.typewrite("cmd")
 pyautogui.press("enter")
 pyautogui.typewrite("jupyter notebook")
 pyautogui.press("enter")
 
 # 開github
-pyautogui.PAUSE = 5
+pyautogui.PAUSE = 20
 pyautogui.press("winleft")
 pyautogui.PAUSE = 0.8
 pyautogui.typewrite("google chrome")
 pyautogui.press("enter")
 
-pyautogui.PAUSE = 1.5
+pyautogui.PAUSE = 20
 pyautogui.moveTo(201,52)
+pyautogui.PAUSE = 0.8  
 pyautogui.click(201,52)
 pyautogui.typewrite("https://github.com/Din1225?tab=repositories")
-pyautogui.PAUSE = 0.8
 pyautogui.press("enter")
 
 # 開Discord
